e75885483f3d6b22816359f3063cde895f0613bb
xyrmovich <danikx511@gmail.com>
2020-08-27 12:29:30 +0300

    Updates

---
+++ precache-manifest.3c7caa145e9c636c4c139b02d43eb32e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e00d0ae81ec2e8524e9870ed4bb8dd",
+    "url": "/UdemyRolodex/index.html"
+  },
+  {
+    "revision": "313d2c91738bad8446c3",
+    "url": "/UdemyRolodex/static/css/main.25fffdd6.chunk.css"
+  },
+  {
+    "revision": "2cc77106302b9b699f2d",
+    "url": "/UdemyRolodex/static/js/2.13c5b67e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/UdemyRolodex/static/js/2.13c5b67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "313d2c91738bad8446c3",
+    "url": "/UdemyRolodex/static/js/main.fe73058e.chunk.js"
+  },
+  {
+    "revision": "ed91530f7f29f07f168e",
+    "url": "/UdemyRolodex/static/js/runtime-main.34453ecb.js"
+  }
+]);
